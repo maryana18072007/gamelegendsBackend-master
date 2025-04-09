daf19cdeffd0c058c29cf6917cd69c677ea67d2d
maryana18072007 <maryanacorinthians123@gmail.com>
2025-04-09 07:58:30 -0300

    GOZAMOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOS

--- src/main/java/br/gamelegends/gamelegends/control/LoginController.java
+++ src/main/java/br/gamelegends/gamelegends/control/LoginController.java
@@ -1,11 +1,18 @@
 package br.gamelegends.gamelegends.control;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.gamelegends.gamelegends.model.Cadastro;
 import br.gamelegends.gamelegends.model.Login;
 import br.gamelegends.gamelegends.service.CadastroService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600, allowCredentials = "false")
